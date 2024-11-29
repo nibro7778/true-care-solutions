b0c8071092b49cddd3464c729b67df1a87f63d5f
nibro7778 <mailme7778@gmail.com>
2024-11-29 17:41:39 +1100

    feat: updated DB password

--- src/Common/Infrastructure/Configuration/DbConfigurationExtensions.cs
+++ src/Common/Infrastructure/Configuration/DbConfigurationExtensions.cs
@@ -7,7 +7,7 @@ public static class DbConfigurationExtensions
     public static string GetDbConnectionString(this IConfiguration configuration, string schema)
     {
         var username = configuration["DB_USERNAME"] ?? "admin";
-        var password = configuration["DB_PASSWORD"] ?? "password";
+        var password = configuration["DB_PASSWORD"] ?? "admin";
         var database = configuration["DB_DATABASE"] ?? "db";
         var host = configuration["DB_HOST"] ?? "localhost";
         var port = configuration["DB_PORT"] ?? "5432";
